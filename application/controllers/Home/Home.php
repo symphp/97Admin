d476462ccbcb9473e8d4b97b3261eaaadffef314
symphp <172344455@qq.com>
2017-07-03 23:03:26 +0800

    增加基础控制器

---
+++ application/controllers/Home/Home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: symphp <symphp@foxmail.com>
+ * Date: 2017/7/3
+ * Time: 22:51
+ */
+class Home extends Home_Controller
+{
+	public function index()
+	{
+
+	}
+}
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: symphp <symphp@foxmail.com>
+ * Date: 2017/7/3
+ * Time: 22:44
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Class Home_Controller
+ * 前台控制器
+ */
+class Home_Controller extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
+
+/**
+ * Class Admin_Controller
+ * 后台控制器
+ */
+class Admin_Controller extends CI_Controller {
+
+}
