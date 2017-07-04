9eb3739deb1d1124cb90ec678565a12cad380077
symphp <symphp@foxmail.com>
2017-07-04 21:59:01 +0800

    小测试

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,6 +18,7 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	
 	public function index()
 	{
 		$this->load->view('welcome_message');
