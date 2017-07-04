410b7caf8dbf1f511be8759297c8bf391490e44f
symphp <symphp@foxmail.com>
2017-07-04 22:54:23 +0800

    测试
    Signed-off-by: symphp <symphp@foxmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,7 +18,6 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	
 	public function index()
 	{
 		$this->load->view('welcome_message');
