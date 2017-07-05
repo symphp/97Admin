45795290d61bdbb77681934bf7843fef163e40ac
symphp <symphp@foxmail.com>
2017-07-05 23:55:56 +0800

    login页面以及创建资源存放目录

---
+++ application/controllers/Admin/Login.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: symphp <symphp@foxmail.com>
+ * Date: 2017/7/5
+ * Time: 22:10
+ */
+class Login extends Admin_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$this->load->view('login');
+	}
+}
---
+++ application/views/Admin/Login.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>97Admin Power by symphp</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+	<link rel="stylesheet" href="../public/../public/assets/bootstrap-3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../public/assets/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../public/css/form-elements.css">
+	<link rel="stylesheet" href="../public/css/style.css">
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+	<!-- Favicon and touch icons -->
+	<link rel="shortcut icon" href="../public/assets/ico/favicon.png">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../public/assets/ico/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../public/assets/ico/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../public/assets/ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="../public/assets/ico/apple-touch-icon-57-precomposed.png">
+
+</head>
+
+<body>
+
+<!-- Top content -->
+<div class="top-content">
+
+	<div class="inner-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 text">
+					<h1><strong>97Admin</strong>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6 col-sm-offset-3 form-box">
+					<div class="form-top">
+						<div class="form-top-left">
+							<h3>Login to 97Admin</h3>
+							<p>Enter your username and password to log on:</p>
+						</div>
+						<div class="form-top-right">
+							<i class="fa fa-key"></i>
+						</div>
+					</div>
+					<div class="form-bottom">
+						<form role="form" action="" method="post" class="login-form">
+							<div class="form-group">
+								<label class="sr-only" for="form-username">Username</label>
+								<input type="text" name="form-username" placeholder="Username..." class="form-username form-control" id="form-username">
+							</div>
+							<div class="form-group">
+								<label class="sr-only" for="form-password">Password</label>
+								<input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password">
+							</div>
+							<div class="form-group">
+								<input type="checkbox" name="remember">Remember me
+							</div>
+							<button type="submit" class="btn">Sign in!</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+<!-- Javascript -->
+<script src="../public/js/jquery.js?v=1"></script>
+<script src="../public/assets/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+<script src="../public/js/jquery.backstretch.min.js"></script>
+<script src="../public/js/scripts.js"></script>
+
+<!--[if lt IE 10]>
+<script src="../public/js/placeholder.js"></script>
+<![endif]-->
+
+</body>
+
+</html>
